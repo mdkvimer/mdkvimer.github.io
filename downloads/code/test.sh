5880ebfb4fdc805ebdcc79a6e456175246e98887
mdkvimer <mengdaikun@gmail.com>
2014-02-18 20:54:41 +0800

    Site updated at 2014-02-18 12:54:41 UTC

---
+++ downloads/code/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: test.sh
+# 
+#         USAGE: ./test.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: mengdaikun (), 
+#  ORGANIZATION: 
+#       CREATED: 02/18/14 20:50:52 CST
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+echo "hahah"
+
